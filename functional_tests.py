45d5988297865557fdda0be4b00ff7b991e50f1a
Kuba <jaknowa2000@gmail.com>
2023-02-16 13:52:07 +0100

    Insert functional_tests into unittest and adding comments with usage scheme.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    # metoda przed każdym testem (tu wybierzemy przegladarke)
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    #metoda po każdym teście  (zamkniecie przegladarki, metoda!)
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # przejście na stronę główą aplikacji
+        self.browser.get('http://localhost:8000')
+        self.assertIn("Listy", self.browser.title)
+        self.fail("Zakonczenie testu")
+
+        # sprawdzenie poprawności tytułu strony
+
+        # wpisanie pierwszego hobby
+
+        # wciśnięcie klawisza enter, wyświetla pierwsze hobby
+
+        # nadal mamy mozliwość wpisania zadania, wpisanie kolejnego zadania
+
+        # wystepuja dwie pozycje na liscie
+
+        # wygenerowany unikalny adres url
+
+        # sprawdzenie tego urla
+
+        # koniec
+
+
+if __name__ == '__main__':
+    unittest.main()
