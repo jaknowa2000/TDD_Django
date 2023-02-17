3aed1b8cd170e6273072090668ab65763d5789e6
Kuba <jaknowa2000@gmail.com>
2023-02-17 13:18:33 +0100

    Functional test also checks whether it is possible to add a row to list.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -8,22 +9,26 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    #metoda po każdym teście  (zamkniecie przegladarki, metoda!)
+    # metoda po każdym teście  (zamkniecie przegladarki, metoda!)
     def tearDown(self) -> None:
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # przejście na stronę główą aplikacji
         self.browser.get('http://localhost:8000')
-        self.assertIn("Listy", self.browser.title)
-        self.fail("Zakonczenie testu")
-
         # sprawdzenie poprawności tytułu strony
-
+        self.assertIn("Listy", self.browser.title)
+        header_text = self.browser.find_element(by="tag name", value="h1").text
+        self.assertIn("Listy", header_text)
         # wpisanie pierwszego hobby
-
+        inputbox = self.browser.find_element(by="id", value='id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Wpisz rzeczy do zrobienia")
+        inputbox.send_keys("Kupić nowe pióra")
         # wciśnięcie klawisza enter, wyświetla pierwsze hobby
-
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element(by="id", value='id_list_table')
+        rows = table.find_elements(by='tag name', value='tr')
+        self.assertTrue(any(row.text == '1: Kupić pawie pióra' for row in rows))
         # nadal mamy mozliwość wpisania zadania, wpisanie kolejnego zadania
 
         # wystepuja dwie pozycje na liscie
@@ -33,6 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # sprawdzenie tego urla
 
         # koniec
+        self.fail("Zakonczenie testu")
 
 
 if __name__ == '__main__':
