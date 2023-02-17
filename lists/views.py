5374e353218ab0afdd2cdb39e8c84f227a76e8ea
Kuba <jaknowa2000@gmail.com>
2023-02-17 12:15:05 +0100

    Simple view returning HTML code for passing test.

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # tu utworzymy widoki
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>Listy rzeczy do zrobienia</title></html>')
