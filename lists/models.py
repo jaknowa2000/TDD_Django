d0fe1f59786c46d039ca949cbe26247c0061c26a
Kuba <jaknowa2000@gmail.com>
2023-02-18 16:53:30 +0100

    Model for object Item and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+# atrybut id w bazie jako klucz gł. jest domyślny reszte trzeba zdefiniowac
+class Item(models.Model):
+    text = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -10,3 +11,23 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'Nowy element listy'})
         self.assertIn('Nowy element listy', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'Absolutnie pierwszy element'
+        # zapisanie w bazie danych
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Drugi element'
+        second_item.save()
+
+        # sprawdzenie co zapisało sie w bazie danych
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element')
+        self.assertEqual(second_saved_item.text, 'Drugi element')
