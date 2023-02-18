505938531d6c2f39efe547157665c5a83b64037e
Kuba <jaknowa2000@gmail.com>
2023-02-18 14:29:38 +0100

    Improved and refactored functional tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from time import sleep
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,27 +14,39 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(by='id', value='id_list_table')
+        rows = table.find_elements(by='tag name', value='tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # przejście na stronę główą aplikacji
         self.browser.get('http://localhost:8000')
+
         # sprawdzenie poprawności tytułu strony
         self.assertIn("Listy", self.browser.title)
         header_text = self.browser.find_element(by="tag name", value="h1").text
         self.assertIn("Twoja lista", header_text)
+
         # wpisanie pierwszego hobby
         inputbox = self.browser.find_element(by="id", value='id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), "Wpisz rzeczy do zrobienia")
-        inputbox.send_keys("Kupić nowe pióra")
+        inputbox.send_keys("Kupić pawie pióra")
+
         # wciśnięcie klawisza enter, wyświetla pierwsze hobby
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element(by="id", value='id_list_table')
-        rows = table.find_elements(by='tag name', value='tr')
-        self.assertTrue(any(row.text == '1: Kupić pawie pióra' for row in rows),
-                        "Nowy element nie znajduje się w tabeli")
+        sleep(3)
+        self.check_for_row_in_list_table('1: Kupić pawie pióra')
+
         # nadal mamy mozliwość wpisania zadania, wpisanie kolejnego zadania
+        inputbox = self.browser.find_element(by='id', value='id_new_item')
+        inputbox.send_keys("Użyć piór do przynęty")
+        inputbox.send_keys(Keys.ENTER)
+        sleep(3)
 
         # wystepuja dwie pozycje na liscie
-
+        self.check_for_row_in_list_table('1: Kupić pawie pióra')
+        self.check_for_row_in_list_table('2: Użyć piór do przynęty')
         # wygenerowany unikalny adres url
 
         # sprawdzenie tego urla
