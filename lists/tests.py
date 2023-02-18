2832eaf2c45a75e85e6e1f8e08a97f66edd8acc5
Kuba <jaknowa2000@gmail.com>
2023-02-18 14:30:07 +0100

    Upgraded unittests for current version of django.

--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,12 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    # sprawdza czy istnieje mapowanie url do widoku home_page
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    # sprawdza czy zadanie zwraca odpowiedni kod przez widok
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        # self.assertTrue(response.content.startswith(b'<!DOCTYPE html>\n<html lang="pl-PL">'))
-        # self.assertIn(b'<title>Listy rzeczy do zrobienia</title>', response.content)
-        # self.assertTrue(response.content.endswith(b'</html>'))
-        # lepsza wersja
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'Nowy element listy'})
+        self.assertIn('Nowy element listy', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
