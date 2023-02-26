0c2c60c47d7093cc56a6d75f7cd8642833ab397d
Kuba <jaknowa2000@gmail.com>
2023-02-26 21:39:49 +0100

    URLs refactoring.

--- lists/urls.py
+++ lists/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 # importuje widoki w mojej apki
 from lists import views
 
 urlpatterns = [
     # utworzenie polaczenia miedzy url '' a widokiem home_page
-    path('', views.home_page, name='home'),
-    path('lists/', include('lists.urls')),
-    # path('admin/', admin.site.urls),
+    path('<str:list_id>/', views.view_list, name='view_list'),
+    path('new', views.new_list, name='new_list'),
+    path('<str:list_id>/add_item', views.add_item, name='add_item')
 ]
