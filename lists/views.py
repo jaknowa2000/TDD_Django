908fb94e7cd6ec45e91c479469a93314cd16e4ef
Kuba <jaknowa2000@gmail.com>
2023-02-28 19:10:10 +0100

    Upgrade models to accept empty null list.

--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,7 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, **kwargs):
-    list_id = kwargs.get('list_id')
+def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     # items = Item.objects.filter(list=list_)  # przekazanie szablonowi elementów z widoku strony głownej
     return render(request, 'list.html', {'list': list_})
@@ -21,8 +20,7 @@ def new_list(request):
     return redirect(f'/lists/{list_.id}/')
 
 
-def add_item(request, **kwargs):
-    list_id = kwargs.get('list_id')
+def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
